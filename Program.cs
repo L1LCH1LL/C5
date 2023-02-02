a9bec3bc96e88c5f0177b45d70fa072b1d7a46ca
L1LCH1LL <108305081+L1LCH1LL@users.noreply.github.com>
2023-02-03 02:29:13 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,27 +1,49 @@
-// 36 Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
-// [3, 7, 23, 12] -> 19
-// [-4, -6, 89, 6] -> 0
+// Задача 38: Задайте массив целых чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
 
 Console.Write("Введите длину массива: ");
 int len = Convert.ToInt32(Console.ReadLine());
 
-
 int Array ()
 {
-    int sum = 0;
-    int[] arr = new int[len];
+
+    int[] arr = new int [len];
     for (int i = 0; i < len; i++)
     {
         arr[i] = new Random().Next(0, len);
-        Console.WriteLine($"Элемент списка: {arr[i]} ---- индекс({i})");
-        if (i % 2 != 0)
+    }
+
+
+    int max = arr[0];
+    for (int i = 0; i < len; i++) // поиск максимального числа
+    {
+        if (arr[i] > max)
         {
-            sum += arr[i];
+            max = arr[i];
+        }
+        else
+        {
+            max = max;
         }
     }
-    return sum;
+
+
+    int min = arr[0];
+    for (int i = 0; i < len; i++) // поиск минимального числа
+    {
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+        else
+        {
+            min = min;
+        }
+    }
+
+    return max - min;
 }
 
+Console.WriteLine(Array());
 
-Console.WriteLine($"Сумма элементов, стоящих на нечётных позициях: {Array()}");
 
